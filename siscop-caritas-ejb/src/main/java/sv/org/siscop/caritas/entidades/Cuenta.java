e1fde4b87b4def2fd2e7033104c8649fad7c56f9
Henry <Henry@192.168.10.1>
2019-07-03 20:56:29 -0600

    Cuenta con pk compuesta

--- siscop-caritas-ejb/src/main/java/sv/org/siscop/caritas/entidades/Cuenta.java
+++ siscop-caritas-ejb/src/main/java/sv/org/siscop/caritas/entidades/Cuenta.java
@@ -7,77 +7,59 @@ package sv.org.siscop.caritas.entidades;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author Leonardo Martinez
+ * @author Henry
  */
 @Entity
 @Table(name = "cuenta")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Cuenta.findAll", query = "SELECT c FROM Cuenta c")})
 public class Cuenta implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "id")
-    private Long id;
-    @Size(max = 50)
-    @Column(name = "codigo")
-    private String codigo;
+    @EmbeddedId
+    protected CuentaPK cuentaPK;
     @Size(max = 100)
     @Column(name = "nombre")
     private String nombre;
     @Size(max = 200)
     @Column(name = "descripcion")
     private String descripcion;
-    @OneToMany(mappedBy = "idctapadre")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "cuentaPadre")
     private List<Cuenta> cuentaList;
-    @JoinColumn(name = "idctapadre", referencedColumnName = "id")
-    @ManyToOne
-    private Cuenta idctapadre;
-    @JoinColumn(name = "idproyecto", referencedColumnName = "id")
-    @ManyToOne
-    private Proyecto idproyecto;
+    @JoinColumns({
+        @JoinColumn(name = "idproyecto", referencedColumnName = "idproyecto", insertable = false, updatable = false)
+        , @JoinColumn(name = "codigopadre", referencedColumnName = "codigo")})
+    @ManyToOne(optional = false)
+    private Cuenta cuentaPadre;
 
     public Cuenta() {
     }
 
-    public Cuenta(Long id) {
-        this.id = id;
+    public Cuenta(CuentaPK cuentaPK) {
+        this.cuentaPK = cuentaPK;
     }
 
-    public Long getId() {
-        return id;
+    public Cuenta(String codigo, long idproyecto) {
+        this.cuentaPK = new CuentaPK(codigo, idproyecto);
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public CuentaPK getCuentaPK() {
+        return cuentaPK;
     }
 
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    public void setCuentaPK(CuentaPK cuentaPK) {
+        this.cuentaPK = cuentaPK;
     }
 
     public String getNombre() {
@@ -96,7 +78,6 @@ public class Cuenta implements Serializable {
         this.descripcion = descripcion;
     }
 
-    @XmlTransient
     public List<Cuenta> getCuentaList() {
         return cuentaList;
     }
@@ -105,26 +86,18 @@ public class Cuenta implements Serializable {
         this.cuentaList = cuentaList;
     }
 
-    public Cuenta getIdctapadre() {
-        return idctapadre;
+    public Cuenta getCuentaPadre() {
+        return cuentaPadre;
     }
 
-    public void setIdctapadre(Cuenta idctapadre) {
-        this.idctapadre = idctapadre;
-    }
-
-    public Proyecto getIdproyecto() {
-        return idproyecto;
-    }
-
-    public void setIdproyecto(Proyecto idproyecto) {
-        this.idproyecto = idproyecto;
+    public void setCuentaPadre(Cuenta cuentaPadre) {
+        this.cuentaPadre = cuentaPadre;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (cuentaPK != null ? cuentaPK.hashCode() : 0);
         return hash;
     }
 
@@ -135,7 +108,7 @@ public class Cuenta implements Serializable {
             return false;
         }
         Cuenta other = (Cuenta) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.cuentaPK == null && other.cuentaPK != null) || (this.cuentaPK != null && !this.cuentaPK.equals(other.cuentaPK))) {
             return false;
         }
         return true;
@@ -143,7 +116,7 @@ public class Cuenta implements Serializable {
 
     @Override
     public String toString() {
-        return codigo+"-"+nombre;
+        return "sv.org.siscop.caritas.entidades.Cuenta[ cuentaPK=" + cuentaPK + " ]";
     }
     
 }
